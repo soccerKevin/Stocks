e496100d1b6a23501e5903f9acb26c71f02b9a74
kevin townsend <kevintownsend@kevins-MacBook-Pro.local>
2021-04-25 20:55:23 -0700

    saving

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
+import routes from './pages';
+
+console.log('routes: ', routes);
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', routes.main)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
