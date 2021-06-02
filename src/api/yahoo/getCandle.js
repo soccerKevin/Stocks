a9812791e28836d9739efd981aeb82109af80ba8
kevin townsend <kevintownsend@kevins-MacBook-Pro.local>
2021-06-02 01:38:55 -0700

    back to yahoo

--- src/api/yahoo/getCandle.js
+++ src/api/yahoo/getCandle.js
@@ -1,23 +1,29 @@
 import moment from 'moment'
 import config from 'stocks/config'
 import { INTERVALS_HASH } from 'stocks/src/conf/intervals'
+import { TICKERS_HASH } from 'stocks/src/conf/tickers'
 
 const { yahoo: { apiKey, apiHost } } = config
 
-export const getOptions = ({ symbol, interval }) => ({
-  baseURL: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com',
-  url:     '/stock/v2/get-chart',
-  params:  {
-    interval: INTERVALS_HASH[interval].yahoo,
-    symbol,
-    range:    '1d',
-    region:   'US',
-  },
-  headers: {
-    'x-rapidapi-key':  apiKey,
-    'x-rapidapi-host': apiHost,
-  },
-})
+export const getOptions = ({ symbol: symb, interval }) => {
+  const { type, yahoo } = TICKERS_HASH[symb]
+  const symbol = type === 'crypto' ? yahoo : symb
+
+  return {
+    baseURL: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com',
+    url:     '/stock/v2/get-chart',
+    params:  {
+      interval: INTERVALS_HASH[interval].yahoo,
+      symbol,
+      range:    '1d',
+      region:   'US',
+    },
+    headers: {
+      'x-rapidapi-key':  apiKey,
+      'x-rapidapi-host': apiHost,
+    },
+  }
+}
 
 export const normalize = (data) => {
   const { timestamp: timestamps, indicators } = data.chart.result[0]
